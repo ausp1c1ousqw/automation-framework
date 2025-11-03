5288f3ef8384e9902e0b96450fab86ad61f24643
Sergey Chernyavsky <qausp1c1ousw@gmail.com>
2025-11-03 20:20:04 +0300

    q

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import { initFramework } from "./utils/initFramework.js";
-
-export { default as Button } from "./core/elements/Button.js";
-export { default as Input } from "./core/elements/Input.js";
-export { default as Checkbox } from "./core/elements/Checkbox.js";
-export { default as Label } from "./core/elements/Label.js";
-export { default as Link } from "./core/elements/Link.js";
-export { default as Dropdown } from "./core/elements/Dropdown.js";
-export { default as BasePage } from "./core/BasePage.js";
+export { default as Button } from "./gui/core/elements/Button.js";
+export { default as Input } from "./gui/core/elements/Input.js";
+export { default as Checkbox } from "./gui/core/elements/Checkbox.js";
+export { default as Label } from "./gui/core/elements/Label.js";
+export { default as Link } from "./gui/core/elements/Link.js";
+export { default as Dropdown } from "./gui/core/elements/Dropdown.js";
+export { default as BasePage } from "./gui/core/BasePage.js";
 export * as initFramework from "./utils/initFramework.js";
-export * as pageHelpers from "./core/pageHelpers.js";
+export * as pageHelpers from "./gui/core/pageHelpers.js";
