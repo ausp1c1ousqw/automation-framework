4bf9d929ffa51d18d7d9377918c0b4bf90998fbf
Sergey Chernyavsky <qausp1c1ousw@gmail.com>
2025-11-05 20:30:23 +0300

    q

--- config/projectConfig.js
+++ config/projectConfig.js
@@ -2,5 +2,9 @@ const config = {
   timeouts: { short: 1000, medium: 2000, long: 5000 },
   baseUrl: null,
   debugDir: "/artifacts",
+  baseApi: "https://reqres.in/api",
+  headers: {
+    "x-api-key": "reqres-free-v1",
+  },
 };
 export default config;
