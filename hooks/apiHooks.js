01b154b7c885037aa5816119ba5bbc4ced08a3d4
Sergey Chernyavsky <qausp1c1ousw@gmail.com>
2025-11-24 22:46:39 +0300

    qwe

--- hooks/apiHooks.js
+++ hooks/apiHooks.js
@@ -4,20 +4,7 @@ import { logger } from "automation-framework/di-container";
 const hooks = {
   before: function () {},
 
-  afterStep: function (step, scenario, { error }) {
-    const stepLogs = logger.getLogs();
-    allure.addAttachment(`Logs for: ${step.text}`, stepLogs, "text/plain");
-
-    logger.clearLogs();
-
-    if (error) {
-      logger.error(error.message || error);
-      throw error;
-    }
-  },
-
-  after: function (world, result) {
-    logger.info(`Scenario Ended: ${world.pickle.name}`);
-  },
+  afterStep: function () {},
+  after: function () {},
 };
 export default hooks;
