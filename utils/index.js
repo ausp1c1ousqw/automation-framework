252d9c2a4ef295799e444ef0dd552056bb444531
Sergey Chernyavsky <qausp1c1ousw@gmail.com>
2025-11-25 20:41:32 +0300

    q

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,3 @@
 export { default as Logger } from "./Logger.js";
 export { default as PageSource } from "./PageSource.js";
 export { default as Screenshot } from "./Screenshot.js";
-export { default as onError } from "./onError.js";
